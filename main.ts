393f21222b7ed10573b1626e24c57e81936b2040
MubashirKhanYousufZai <161746933+MubashirKhanYousufZai@users.noreply.github.com>
2024-03-12 07:12:12 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,10 @@
+let guestName =["Mubashir","Ali","Aliyan"];
+let main =("you are invited");
+let invitation =("\nplease join us for rehearsel Dinner Honouring\n");
+let time =("\n8:00\n");
+let date =("\n8-April-2024\n");
+let place =("Taj Hotel");
+let mainMessage =("\nMubashir,Ali or Aliyan are also invited. \n")
+console.log(guestName[0],main,invitation,time,date,place,mainMessage);
+console.log(guestName[1],main,invitation,time,date,place,mainMessage);
+console.log(guestName[2],main,invitation,time,date,place,mainMessage);
